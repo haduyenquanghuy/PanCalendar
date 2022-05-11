77c17416f73a950507d38dcca38ecdb5924d25ed
Quang Huy Ha Duyen <haduyenquanghuy@gmail.com>
2022-05-11 20:35:38 +0700

    change navigationBar item (back, title) button color and segment bar item color

--- PanCalendar/AppDelegate.swift
+++ PanCalendar/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
+        
+        let font = UIFont(name: "Gill Sans", size: 16.0)!          // Compute the right size
+        UISegmentedControl.appearance().setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
         return true
     }
     
